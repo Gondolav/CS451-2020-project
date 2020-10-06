2b140876dda4659fdb2f251fe2904a9034054fa8
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-10-06 10:23:03 +0200

    Fix

--- tests/test_perfect_links.py
+++ tests/test_perfect_links.py
@@ -34,20 +34,20 @@ for key, value in logs.items():
     logs_pld = list(filter(lambda l: l[0] == 'd' in l, value))
     print(logs_pld)
     s = set([x for x in logs_pld if logs_pld.count(x) > 1])
-    soft_assert(0 == len(s), "Some messages have been delived more than once : {}".format(s))
+    soft_assert(0 == len(s), "Some messages have been delivered more than once : {}".format(s))
 
 # ### No creation - No message is delivered unless it was sent
 for key, value in logs.items():
     logs_pld = list(filter(lambda l: l[0] == 'd' in l, value))
     for d in logs_pld:
-        soft_assert(any(l[0] == 'b' and l[2:] == d[1:] for l in logs[int(d[2])]), "Message {} is delivered while not sent ".format(d))
+        soft_assert(any(l[0] == 'b' and l[1:] == d[2:] for l in logs[int(d[1])]), "Message {} is delivered while not sent ".format(d))
         # soft_assert(['pls', d[1], d[2], d[3]] in logs[int(d[2])], "Message {} is delivered while not sent ".format(d))
         
 # ### Validity - If pi and pj are correct, then every message sent by pi to pj is eventually delivered by pj
 for key, value in logs.items():
     logs_pls = list(filter(lambda l: l[0] == 'b' in l, value))
     for m in logs_pls:
-        soft_assert(['d', d[1], d[2], d[3]] in logs[int(d[1])], "Message {} is never delivered".format(m))
+        soft_assert(['d', m[1], key] in logs[int(m[1])], "Message {} is never delivered".format(m))
 
 # printing the last line with status
 print("INCORRECT" if were_errors else "CORRECT")
