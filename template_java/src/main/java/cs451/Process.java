66b10745d3ea4b5abe615ebc33a926da0c39cd41
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-10-10 14:50:11 +0200

    Test FIFO

--- template_java/src/main/java/cs451/Process.java
+++ template_java/src/main/java/cs451/Process.java
@@ -1,6 +1,6 @@
 package cs451;
 
-import cs451.broadcast.UniformReliableBroadcast;
+import cs451.broadcast.FIFOBroadcast;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -19,7 +19,7 @@ public class Process implements Observer {
 
     private final ConcurrentLinkedQueue<String> logs;
 
-    private final UniformReliableBroadcast broadcast;
+    private final FIFOBroadcast broadcast;
 
     public Process(int id, int port, int nbMessagesToBroadcast, List<Host> hosts, String output) {
         this.id = id;
@@ -27,7 +27,7 @@ public class Process implements Observer {
         this.totalNbMessagesInQueue = nbMessagesToBroadcast * hosts.size(); // add +1 when using BEB
         this.output = output;
         this.logs = new ConcurrentLinkedQueue<>();
-        this.broadcast = new UniformReliableBroadcast(this, hosts, port, id);
+        this.broadcast = new FIFOBroadcast(this, hosts, port, id);
     }
 
     public void startBroadcasting() {
